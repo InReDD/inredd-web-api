6c77d3b5270ab4795cf6ab4cd8f929caa14162fe
Caio Uehara <caioueharamartins@gmail.com>
2025-04-03 13:55:46 -0300

    Remove python project

--- configs/docker/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SERVERNAME=$(cat /etc/hostname)
-export SERVERNAME
-
-uwsgi --socket 0.0.0.0:18082 --protocol=http -w wsgi:app --enable-threads --workers 16 --websockets-max-size 16384
-
-exec "$@"
